d4f81b2ab8ad6f78e2d50418ff0f330d6f7bab6f
Craig Bakke <casbakke@gmail.com>
2026-02-15 20:39:05 -0500

    Update Question 1.sql

---
+++ Module 3/Question 1.sql
@@ -0,0 +1,10 @@
+/*
+Write a SELECT statement that returns four columns from the Products table:
+product_code, product_name, list_price, and discount_percent.
+Add an ORDER BY clause to this statement that sorts the result set by
+list price in descending sequence. 
+*/
+
+SELECT product_code, product_name, list_price, discount_percent
+FROM Products
+ORDER BY list_price DESC;
